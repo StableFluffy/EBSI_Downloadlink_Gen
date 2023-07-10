d70a4d612a02f1a9fad6bf36e6500c17b5795fcf
maywell <92216445+Kon-UU@users.noreply.github.com>
2023-07-11 00:29:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,70 @@
+from lxml import etree
 import requests
 import re
 
 URL = "https://www.ebsi.co.kr/ebs/xip/xipc/previousPaperListAjax.ajax"
-PAGES = 4
+STARTPAGE = 1
+PAGES = 14
+
+print("[")
 
 c_header = {
-    'referer' : 'https://www.ebsi.co.kr/ebs/xip/xipc/previousPaperList.ebs?targetCd=D300',
+    'referer' : 'https://www.ebsi.co.kr/ebs/    xip/xipc/previousPaperList.ebs?targetCd=D100',
     'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36' 
 }
 
-def req_eng(pagecount):
-
+def req(pagecount):
 
     c_data = {
-        'targetCd': 'D100',
+        'targetCd': 'D300',
         'monthList': '03,02,04,05,06,07,09,10,11,12',
-        'subjList': '3',
         'sort': 'recent',
-        #'paperId',
-        #'paperNo',
-        #'lvl',
+        'pageSize': '50',
         'beginYear': '2016',
-        'endYear': '2022',
-        'subj': '3',
+        'endYear': '2023',
+        'subjList': '1',
         'currentPage': pagecount
     }
 
     r = requests.post(URL, headers = c_header, data = c_data)
     t_v = r.text
+    #print(t_v)
+    tree = etree.HTML(t_v)
+    t_path = (tree.xpath('//*[@id="pagingForm"]/div[2]/ul/li'))
+    print(len(t_path))
+    for btn_data in t_path:
+        parsed = ((etree.HTML(etree.tostring(btn_data, method='html', encoding='unicode'))).xpath('/html/body/li/div[3]/div[1]')[0])
+        parsed_str = (etree.tostring(parsed, method='html', encoding='unicode'))
+        try:
 
-    hits = re.findall(r"goDownLoadP.+?pdf", t_v)
-    for hit in hits:
-        print("https://wdown.ebsi.co.kr/W61001/01exam" + hit.split("'")[1])
+            print(((re.findall(r"goDownLoadP.+?;", parsed_str)[0])[12:-2]).split(',')[0][1:-1])
+            print(((re.findall(r"goDownLoadJ.+?;", parsed_str)[0])[12:-2]).split(',')[0][1:-1])
+        except:
+            None
 
-for i in range(PAGES):
-    req_eng(i + 1)
+    
+    hits_n = re.findall(r"paperOn.+?;", t_v)
+    #print(hits_n)
+    hits_a = re.findall(r"goDownLoadP.+?pdf", t_v)
+
+    hits_b = re.findall(r"goDownLoadH.+?pdf", t_v)
+    #for c in range(len(hits_n)):
+    #    print("{")
+    #    print('"ym" : "' + (hits_a[c].split("'")[1])[1:7] + '",')
+    #    print('"fullname" : "' + hits_n[c].split(",")[1] + '",')
+    #    print('"munlink" : "' + hits_a[c].split("'")[1] + '",')
+    #    print('"hsjlink" : "' + hits_b[c].split("'")[1] + '"')
+    #    print("},")
+    #for hit in hits_n:
+    #    print(hit.split(",")[1])
+    #hits_a = re.findall(r"goDownLoadP.+?pdf", t_v)
+    #for hit in hits_a:
+    #    print(hit.split("'")[1])
+    #hits_b = re.findall(r"goDownLoadH.+?pdf", t_v)
+    #for hit in hits_b:
+    #    print(hit.split("'")[1])
+
+for i in range(PAGES - STARTPAGE + 1):
+    req(i + STARTPAGE)
+
+print("]")
